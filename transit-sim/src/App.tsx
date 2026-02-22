aeafc85952c5c0c604635af4e4eb08e92dbf97fe
Anthony <92795688+lausan3@users.noreply.github.com>
2026-02-22 18:23:00 -0500

    feat: add transit map component and minor: change page title (#2)
    * feat: change page title to NYC Transit Traffic Simulator
    
    * feat: add TransitMap component

--- transit-sim/src/App.tsx
+++ transit-sim/src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import TransitMap from "./components/map";
 
 function App() {
   return (
     <>
       <h1>NYC Transit Traffic Simulation</h1>
       <p>A project to visualize the system bottlenecks of the NYC subway.</p>
+
+      <TransitMap />
     </>
   );
 }
