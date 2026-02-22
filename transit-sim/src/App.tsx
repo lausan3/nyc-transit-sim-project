033fdf126d8c536c666a50f1bbce8edfee7689b3
Anthony <92795688+lausan3@users.noreply.github.com>
2026-02-22 18:03:22 -0500

    feat: initialize react project (#1)

---
+++ transit-sim/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>NYC Transit Traffic Simulation</h1>
+      <p>A project to visualize the system bottlenecks of the NYC subway.</p>
+    </>
+  );
+}
+
+export default App;
